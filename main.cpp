b0eafe6e441ef5802e859fdb2f2a014147eb86e7
Yichao Zhong <89649996+z-taylcr7@users.noreply.github.com>
2022-07-04 08:50:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(){
 	std::ios::sync_with_stdio(false);
     std::cin.tie(0);
     std::cout.tie(0);
-    freopen("az.out","w",stdout);
+ //   freopen("az.out","w",stdout);
 //	std::ifstream in("3.txt");
 //    std::streambuf *backinbuf = std::cin.rdbuf();
 //    std::cin.rdbuf(in.rdbuf()); 
